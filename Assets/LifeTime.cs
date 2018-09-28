ba3fed98fb8bc4fa9a6b7959126b5206f7f68e5e
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-09-28 09:53:09 -0400

    Finished class 9/28
    Working on turret math. Will require a detailed review of Trig

---
+++ Assets/LifeTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour {
+
+    public float lifetime = 3;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("KillMe", lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+    void KillMe()
+    {
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Turret.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public GameObject Bullet;
+    public float fireRate = 3;
+
+    float lastFireTime = float.MinValue;
+
+    /*
+     SOH CAH TOA
+     Sine opposite over hypotenuse
+     Cosine adjacent over hypotenuse
+     Tangent opposite over adjacent
+     O - Theta = Angle
+     Tan(O) = Opposite/adjacent
+     0 = ArcTan( opposite/adjacent)_  
+     ArcTan return only 0-90 degrees
+     Most math libraries/functions/game engines have Atan2
+         * 
+         * 
+         */
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Mathf.Atan2()
+	    if(Time.time - (1/fireRate) > lastFireTime)
+        {
+            Instantiate(Bullet, transform.position, transform.rotation);
+            lastFireTime = Time.time;
+        }
+	}
+}
